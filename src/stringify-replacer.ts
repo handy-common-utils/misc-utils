819c48566b5fca137b6e4f798160c37ae48585db
James Hu <james.hu.ustc@hotmail.com>
2023-03-19 12:44:00 +0000

    finish up pathAwareReplacer

--- src/stringify-replacer.ts
+++ src/stringify-replacer.ts
@@ -1,13 +1,40 @@
-type Index = string|number;
-type JsonStringifyReplacer = (this: any, key: string, value: any) => any;
-export type PathAwareReplacer = (key: Index, value: any, path: string, obj: any, pathArray: ReadonlyArray<Index>, ancestors: ReadonlyArray<any>) => any;
+type Parent = Record<string|number|symbol, unknown>|Array<unknown>;
+/**
+ * The original replacer expected by JSON.stringify(...)
+ */
+type JsonStringifyReplacer<I = string> = (this: any, key: I, value: any) => any;
+
+/**
+ * The replacer that can potentially utilise the full path of the property in the object.
+ * @param I type of the property names.
+ *          It is `string` by default, which makes it aligned with JSON.stringify(...)
+ *          It could be specified as `string|number` which Is a workaround to this issue: https://github.com/BridgeAR/safe-stable-stringify/issues/42
+ * @param key Name of the property, or the index in the parent array.
+ * @param value Value of the property or the object in the parent array.
+ * @param path The full path of the property in the object, such like "access.visitor.location" or "request.x-forwarded-for.0".
+ *             Please note that the special characters (including ".") in property names are not escaped, for example, "order.billing address.first line".
+ * @param parent The object that the property or the element belongs to. It could be `{ '': <the original object> }` when this replacer function is called the first time.
+ * @param pathArray The full path as an array. It is more useful than `path` in case special characters exist in property names.
+ *                  When this replacer function is called the first time, pathArray array would have a zero length.
+ * @param ancestors All the ancestor objects/arrays of the property.
+ *                  When this replacer function is called the first time, ancestors array would have a zero length.
+ */
+export type PathAwareReplacer<I = string> = (key: I, value: any, path: string, parent: Parent, pathArray: Array<I>, ancestors: Array<Parent>) => any;
 
 const currentPath: any[] = [];
 const ancestors: any[] = [];
 
-export function pathAwareReplacer(replacer: PathAwareReplacer): JsonStringifyReplacer {
-  return function (this: any, key: string, value: any): any {
-    if (key === '') {   // root object
+/**
+ * Build a replacer function that can be passed to JSON.stringify(...).
+ * @param replacer The actual replacer function which could utilise additional information.
+ * @param options Options to control whether the pathArray and ancestors parameters would have values populated.
+ *                By default all information available would be populated.
+ *                There is no need to specify options unless you are extremely concerned about performance, for example if you need to frequently stringify 500MB objects.
+ * @returns The replacer function that can be passed to JSON.stringify(...).
+ */
+export function pathAwareReplacer<I = string>(replacer: PathAwareReplacer<I>, options?: {pathArray?: boolean, ancestors?: boolean}): JsonStringifyReplacer<I> {
+  return function (this: any, key: I, value: any): any {
+    if (typeof key === 'string' && key === '') {   // root object
       currentPath.length = 0;
       ancestors.length = 0;
     } else {
@@ -24,6 +51,13 @@ export function pathAwareReplacer(replacer: PathAwareReplacer): JsonStringifyRep
       }
       currentPath.push(key);
     }
-    return replacer(key, value, currentPath.join('.'), this, currentPath, ancestors);
+    return replacer(
+      key,
+      value,
+      currentPath.join('.'),
+      this,
+      options?.pathArray === false ? [] : [...currentPath],
+      options?.ancestors === false ? [] : [...ancestors],
+    );
   };
 }
--- test/stringify-replacer.spec.ts
+++ test/stringify-replacer.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable unicorn/no-for-loop */
+/* eslint-disable @typescript-eslint/ban-types */
 import { expect } from 'chai';
 import stringify from 'safe-stable-stringify';
 import { pathAwareReplacer } from '../src/stringify-replacer';
@@ -33,67 +35,161 @@ const obj = {
 };
 o2.parent = obj;
 
+const expectedRoundTripObj = { ...obj };
+expectedRoundTripObj.aObj.child.o2.parent = '[Circular]';
+delete expectedRoundTripObj.un;
+
+const expectedParamKeys = [
+  '',
+  'aObj',
+  'child',
+  'ip',
+  'location',
+  'o2',
+  'name',
+  'parent',
+  'name',
+  'name with spaces',
+  'name.with.dots',
+  'x',
+  'y',
+  'age',
+  'arr',
+  0,
+  1,
+  2,
+  'color',
+  'shape',
+  'nu',
+  'un',
+];
+
+const expectedParamPaths = [
+  '',
+  'aObj',
+  'aObj.child',
+  'aObj.child.ip',
+  'aObj.child.location',
+  'aObj.child.o2',
+  'aObj.child.o2.name',
+  'aObj.child.o2.parent',
+  'aObj.name',
+  'aObj.name with spaces',
+  'aObj.name.with.dots',
+  'aObj.x',
+  'aObj.y',
+  'age',
+  'arr',
+  'arr.0',
+  'arr.1',
+  'arr.2',
+  'arr.2.color',
+  'arr.2.shape',
+  'nu',
+  'un',
+];
+
+const stringifyKeys: Array<string|number> = [];
+const stringifyParents: Array<any> = [];
+stringify(obj, function (this: any, key: string|number, value: any) {
+  stringifyKeys.push(key);
+  stringifyParents.push(this);
+  return value;
+});
+
+// const jsonStringifyKeys: Array<string|number> = [];
+// const jsonStringifyParents: Array<any> = [];
+// JSON.stringify(obj, function (this: any, key: string|number, value: any) {
+//   jsonStringifyKeys.push(key);
+//   jsonStringifyParents.push(this);
+//   return value;
+// });
+// console.log(jsonStringifyKeys);
+
 describe('stringifyReplacer(...)', () => {
   it('should supply the correct parameters to encapsulated replacer function', () => {
     const keys: Array<string|number> = [];
     const paths: string[] = [];
-    const s = stringify(obj, pathAwareReplacer((key: string|number, value: any, path: string, _obj: any) => {
+    const parents: Array<object|Array<unknown>> = [];
+    const pathArrays: Array<Array<any>> = [];
+    const allAncestors: Array<Array<any>> = [];
+    const s = stringify(obj, pathAwareReplacer((key: string|number, value: any, path: string, parent: any, pathArray: Array<any>, ancestors: Array<any>) => {
+      keys.push(key);
+      paths.push(path);
+      parents.push(parent);
+      pathArrays.push(pathArray);
+      allAncestors.push(ancestors);
+      return value;
+    }), 2);
+
+    expect(s).to.be.a('string');
+    expect(JSON.parse(s!)).to.deep.equal(expectedRoundTripObj);
+
+    expect(paths[0]).to.equal('');
+    expect(paths[1]).to.equal('aObj');
+    expect(parents[0]).to.deep.equal({ '': obj });
+    expect(parents[1]).to.equal(obj);
+
+    expect(pathArrays[0].length).to.equal(0);
+    expect(pathArrays[1].length).to.equal(1);
+    expect(pathArrays[1]).to.deep.equal(['aObj']);
+    expect(pathArrays[2].length).to.equal(2);
+    expect(pathArrays[2]).to.deep.equal(['aObj', 'child']);
+
+    for (let i = 0; i < pathArrays.length; i++) {
+      for (let j = 0; j < pathArrays[i].length; j++) {
+        expect(paths[i]).to.include(pathArrays[i][j]);
+      }
+    }
+
+    expect(allAncestors[0].length).to.equal(0);
+    expect(allAncestors[1].length).to.equal(1);
+    expect(allAncestors[1][0]).to.equal(obj);
+    expect(allAncestors[2].length).to.equal(2);
+    expect(allAncestors[2][0]).to.equal(obj);
+    expect(allAncestors[2][1]).to.equal(obj.aObj);
+    for (let i = 1; i < allAncestors.length; i++) {
+      expect(allAncestors[i][0]).to.equal(obj);
+    }
+
+    expect(keys).to.deep.equal(stringifyKeys);
+    expect(parents).to.deep.equal(stringifyParents);
+
+    expect(keys).to.deep.equal(expectedParamKeys);
+    expect(paths).to.deep.equal(expectedParamPaths);
+  });
+
+  it('should be able to use options', () => {
+    const keys: Array<string|number> = [];
+    const paths: string[] = [];
+    const s = stringify(obj, pathAwareReplacer((key: string|number, value: any, path: string) => {
       keys.push(key);
       paths.push(path);
       return value;
-    }), 2);
+    }, { pathArray: false, ancestors: false }), 2);
+
     expect(s).to.be.a('string');
-    const expected = { ...obj };
-    expected.aObj.child.o2.parent = '[Circular]';
-    delete expected.un;
-    expect(JSON.parse(s!)).to.deep.equal(expected);
-    expect(keys).to.deep.equal([
-      '',
-      'aObj',
-      'child',
-      'ip',
-      'location',
-      'o2',
-      'name',
-      'parent',
-      'name',
-      'name with spaces',
-      'name.with.dots',
-      'x',
-      'y',
-      'age',
-      'arr',
-      0,
-      1,
-      2,
-      'color',
-      'shape',
-      'nu',
-      'un',
-    ]);
-    expect(paths).to.deep.equal([
-      '',
-      'aObj',
-      'aObj.child',
-      'aObj.child.ip',
-      'aObj.child.location',
-      'aObj.child.o2',
-      'aObj.child.o2.name',
-      'aObj.child.o2.parent',
-      'aObj.name',
-      'aObj.name with spaces',
-      'aObj.name.with.dots',
-      'aObj.x',
-      'aObj.y',
-      'age',
-      'arr',
-      'arr.0',
-      'arr.1',
-      'arr.2',
-      'arr.2.color',
-      'arr.2.shape',
-      'nu',
-      'un',
-    ]);
+    expect(JSON.parse(s!)).to.deep.equal(expectedRoundTripObj);
+
+    expect(keys).to.deep.equal(expectedParamKeys);
+    expect(paths).to.deep.equal(expectedParamPaths);
+  });
+
+  it('should honour options', () => {
+    const keys: Array<string|number> = [];
+    const paths: string[] = [];
+    const s = stringify(obj, pathAwareReplacer((key: string|number, value: any, path: string, _parent: any, pathArray: Array<any>, ancestors: Array<any>) => {
+      keys.push(key);
+      paths.push(path);
+      expect(pathArray.length).to.equal(0);
+      expect(ancestors.length).to.equal(0);
+      return value;
+    }, { pathArray: false, ancestors: false }), 2);
+
+    expect(s).to.be.a('string');
+    expect(JSON.parse(s!)).to.deep.equal(expectedRoundTripObj);
+
+    expect(keys).to.deep.equal(expectedParamKeys);
+    expect(paths).to.deep.equal(expectedParamPaths);
   });
 });
