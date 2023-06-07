f08acb3e6aae275f88cf405b924bc46803e48979
James Hu <james.hu.ustc@hotmail.com>
2023-06-07 22:44:45 +1000

    fix: optimised performance 3x

--- test/array.spec.ts
+++ test/array.spec.ts
@@ -2,12 +2,12 @@ import { expect } from 'chai';
 import { distributeRoundRobin } from '../src';
 
 describe('distributeRoundRobin()', () => {
-  it('should work when number of groups equal number of elements', () => {
+  it('should work when number of groups equals number of elements', () => {
     const items = [1, 2, 3, 4, 5];
     const result = distributeRoundRobin(items, 5);
     expect(result).to.deep.equal([[1], [2], [3], [4], [5]]);
   });
-  it('should work when number of groups equal to half of the number of elements', () => {
+  it('should work when number of groups equals to half of the number of elements', () => {
     const items = [1, 2, 3, 4, 5, 6];
     const result = distributeRoundRobin(items, 3);
     expect(result).to.deep.equal([[1, 4], [2, 5], [3, 6]]);
@@ -42,4 +42,27 @@ describe('distributeRoundRobin()', () => {
     const result = distributeRoundRobin(items, -1);
     expect(result).to.deep.equal([]);
   });
+  // for (const [size, groups] of [[10000, 7], [100000, 7], [100000, 77], [1000000, 7], [10000000, 7], [10000000, 333]]) {
+  //   it(`should have good performance for distributing array of size ${size} into ${groups} groups`, function () {
+  //     this.timeout(20000);
+  //     // eslint-disable-next-line unicorn/no-new-array
+  //     const array = new Array(size).map((_, i) => `String at position ${i}`);
+  //     const repeat = 30;
+  //     let totalDuration = 0;
+  //     let totalDurationWithoutOptimisation = 0;
+
+  //     for (let i = 0; i < repeat; i++) {
+  //       let startTime = Date.now();
+  //       let result = distributeRoundRobin(array, groups);
+  //       totalDuration += Date.now() - startTime;
+  //       expect(result.length).to.equal(groups);
+
+  //       startTime = Date.now();
+  //       result = distributeRoundRobinWithoutOptimisation(array, groups);
+  //       totalDurationWithoutOptimisation += Date.now() - startTime;
+  //       expect(result.length).to.equal(groups);
+  //     }
+  //     console.log(`${size} / ${groups} => ${(totalDuration / repeat).toFixed(1)}ms vs. ${(totalDurationWithoutOptimisation / repeat).toFixed(1)}ms`);
+  //   });
+  // }
 });
