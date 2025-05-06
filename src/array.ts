e4a8093ce24145fb1077f2c936130ef901b4ef4f
James Hu <james.hu.ustc@hotmail.com>
2025-05-06 22:12:32 +1000

    feat: add function downSampleRandomly

--- src/array.ts
+++ src/array.ts
@@ -42,3 +42,38 @@ export function distributeRoundRobin<T>(array: Array<T>, groups: number): Array<
 //   }
 //   return result;
 // }
+
+/**
+ * Down samples the input array randomly.
+ * @param array The input array
+ * @param numSamples Number of samples to be taken from the input array.
+ *                  If the number of samples is greater than or equal to the length of the input array,
+ *                  the output array will contain all the elements in the input array.
+ * @param probabilityTransformerFunction A function that turns a random number within [0, 1) to another number within [0, 1).
+ *                If not provided, the identity function F(x) = x will be used.
+ *                The probability of an element being selected from the input array is determined by this function.
+ * @returns A new array with the down sampled elements from the input array.
+ *          The order of the elements in the output array is the same as the input array.
+ */
+export function downSampleRandomly<T>(array:  Array<T>, numSamples: number, probabilityTransformerFunction: ((x: number) => number) = x => x): Array<T> {
+  if (numSamples <= 0) {
+    return [];
+  }
+  if (numSamples >= array.length) {
+    return [...array];
+  }
+
+  const result: Array<T> = [];
+  const indexes = new Set<number>();
+  while (indexes.size < numSamples) {
+    const r = Math.random();
+    indexes.add(Math.floor(probabilityTransformerFunction(r) * array.length));
+  }
+  // eslint-disable-next-line unicorn/no-for-loop
+  for (let i = 0; i < array.length; i++) {
+    if (indexes.has(i)) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
--- test/array.spec.ts
+++ test/array.spec.ts
@@ -1,5 +1,7 @@
+/* eslint-disable unicorn/no-array-for-each */
 import { expect } from 'chai';
 import { distributeRoundRobin } from '../src';
+import { downSampleRandomly } from '../src';
 
 describe('distributeRoundRobin()', () => {
   it('should work when number of groups equals number of elements', () => {
@@ -65,4 +67,55 @@ describe('distributeRoundRobin()', () => {
   //     console.log(`${size} / ${groups} => ${(totalDuration / repeat).toFixed(1)}ms vs. ${(totalDurationWithoutOptimisation / repeat).toFixed(1)}ms`);
   //   });
   // }
+});
+
+describe('downSampleRandomly()', () => {
+  it('should return an empty array when numSamples is 0', () => {
+    const items = [1, 2, 3, 4, 5];
+    const result = downSampleRandomly(items, 0);
+    expect(result).to.deep.equal([]);
+  });
+
+  it('should return the same array when numSamples is greater than or equal to the array length', () => {
+    const items = [1, 2, 3, 4, 5];
+    const result = downSampleRandomly(items, 5);
+    expect(result).to.have.members(items);
+    expect(result.length).to.equal(items.length);
+  });
+
+  it('should return a subset of the array when numSamples is less than the array length', () => {
+    const items = [1, 2, 3, 4, 5];
+    const result = downSampleRandomly(items, 3);
+    expect(result.length).to.equal(3);
+    result.forEach(item => expect(items).to.include(item));
+  });
+
+  it('should respect the probabilityTransformerFunction', () => {
+    const items = [1, 2, 3, 4, 5];
+    // eslint-disable-next-line unicorn/consistent-function-scoping
+    const transformer = (x: number) => x / 2; // Bias towards the first half of the array
+    const result = downSampleRandomly(items, 3, transformer);
+    expect(result.length).to.equal(3);
+    expect(result).to.deep.equal([1, 2, 3]); // Expecting the first three elements to be selected
+  });
+
+  it('should work with an empty array', () => {
+    const items: number[] = [];
+    const result = downSampleRandomly(items, 3);
+    expect(result).to.deep.equal([]);
+  });
+
+  it('should not modify the input array', () => {
+    const items = [1, 2, 3, 4, 5];
+    const original = [...items];
+    downSampleRandomly(items, 3);
+    expect(items).to.deep.equal(original);
+  });
+
+  it('should return unique elements in the result', () => {
+    const items = [1, 2, 3, 4, 5];
+    const result = downSampleRandomly(items, 3);
+    const uniqueItems = new Set(result);
+    expect(uniqueItems.size).to.equal(result.length);
+  });
 });
